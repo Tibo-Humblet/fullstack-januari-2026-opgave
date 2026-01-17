26367761099679191e0b64947f0cb4598c8eeac1
Tibo-Humblet <tibohumblet@hotmail.com>
2026-01-17 13:53:54 +0000

    updated back-end - updated teacher model, repository, service, controller - now able to get a list of all teacher

--- back-end/controller/teacher.routes.ts
+++ back-end/controller/teacher.routes.ts
@@ -34,7 +34,11 @@ const teacherRouter = express.Router();
  *               items:
  *                  $ref: '#/components/schemas/Teacher'
  */
-teacherRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {});
+teacherRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.status(200).json(await teacherService.getAllTeachers());
+    } catch (error) { next(error); }
+});
 
 /**
  * @swagger
--- back-end/model/teacher.ts
+++ back-end/model/teacher.ts
@@ -15,6 +15,8 @@ export class Teacher {
         updatedAt?: Date;
         learningPath: string;
     }) {
+        this.validate(teacher);
+
         this.id = teacher.id;
         this.user = teacher.user;
         this.createdAt = teacher.createdAt;
@@ -22,7 +24,28 @@ export class Teacher {
         this.learningPath = teacher.learningPath;
     }
 
-    static from() {
-        return null;
+    // start getters
+        getId(): number | undefined { return this.id; }
+        getUser(): User { return this.user; }
+        getCreatedAt(): Date { return this.createdAt; }
+        getUpdatedAt(): Date { return this.createdAt; }
+        getLearningPath(): string { return this.learningPath; }
+    // end getters
+
+    // start functions
+        validate(teacher: { user: User; learningPath: string; }) {
+            if (!teacher.user) { throw new Error('user is required'); }
+            if (!teacher.learningPath) { throw new Error('learningPath of theacher is required'); }
+        }
+    // end functions
+
+    static from({id, createdAt, updatedAt, learningPath, user}: TeacherPrisma & { user: UserPrisma; }) {
+        return new Teacher({
+            id: id,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            learningPath: learningPath,
+            user: User.from(user),
+        });
     }
 }
